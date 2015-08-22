5590c433fb6d19c54e52b644cb6212bddc278230
Jani Rahkola <jani.rahkola@iki.fi>
2015-08-22 21:49:28 +0300

    docopt

--- sacret.py
+++ sacret.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
+"""Usage:
+  sacret.py (init | list) [--secrets <dir>]
+  sacret.py (show | copy | edit) [--secrets <dir>] <secret>
+  sacret.py (-h | --help | -v | --version)
 
-import argparse
+Options:
+  -s <dir> --secrets <dir>  Directory of secrets. [default: ~/.sacret]
+  -h --help                 Show this help.
+  -v --version              Show version.
+
+"""
 import base64
+from docopt import docopt
 import hashlib
 import os
 import os.path
@@ -13,10 +23,10 @@ import tempfile
 class Index(object):
     INDEX = "index.asc"
 
-    def __init__(self, salt, names, sacret_dir):
+    def __init__(self, salt, names, directory):
         self.salt = salt
         self.names = names
-        self.sacret_dir = sacret_dir
+        self.directory = directory
         self.modified = False
 
     def __len__(self):
@@ -38,7 +48,7 @@ class Index(object):
         if exc_type is None and self.modified:
             r = self.to_disk()
             if r != 0:
-                path = os.path.join(self.sacret_dir, self.__class__.INDEX)
+                path = os.path.join(self.directory, self.__class__.INDEX)
                 cmd = ["gpg", "-q", "-e", "-a", "--output", path]
                 raise subprocess.CalledProcessError(r, cmd)
 
@@ -46,12 +56,11 @@ class Index(object):
         return self.names.keys()
 
     def add(self, name):
-        self.names[name] = os.path.join(self.sacret_dir,
-                                        hash_name(name, self.salt))
+        self.names[name] = os.path.join(self.directory, hash_name(name, self.salt))
         self.modified = True
 
     def to_disk(self):
-        path = os.path.join(self.sacret_dir, self.__class__.INDEX)
+        path = os.path.join(self.directory, self.__class__.INDEX)
         with subprocess.Popen(["gpg", "-q", "-e", "-a", "--output", path],
                               universal_newlines=True,
                               stdin=subprocess.PIPE) as gpg:
@@ -61,23 +70,23 @@ class Index(object):
             return r
 
     @classmethod
-    def from_disk(cls, sacret_dir):
-        path = os.path.join(sacret_dir, cls.INDEX)
+    def from_disk(cls, directory):
+        path = os.path.join(directory, cls.INDEX)
         salt, *names = subprocess.check_output(["gpg", "-q", "-d", path],
                                                universal_newlines=True).splitlines()
         return cls(salt,
-                   {name: os.path.join(sacret_dir, hash_name(name, salt))
+                   {name: os.path.join(directory, hash_name(name, salt))
                     for name in names},
-                   sacret_dir)
+                   directory)
 
     @classmethod
-    def init(cls, sacret_dir):
-        path = os.path.join(sacret_dir, cls.INDEX)
+    def init(cls, directory):
+        path = os.path.join(directory, cls.INDEX)
         if os.path.exists(path):
             print("index file {} exists".format(path), file=sys.stderr)
             return 1
         salt = base64.urlsafe_b64encode(os.urandom(16)).decode("utf-8")
-        return cls(salt, {}, sacret_dir).to_disk()
+        return cls(salt, {}, directory).to_disk()
 
 
 def hash_name(name, salt):
@@ -85,15 +94,16 @@ def hash_name(name, salt):
     return base64.urlsafe_b64encode(hashlib.sha256(bytes).digest()).decode("utf-8")
 
 def list_secrets(args):
-    print("\n".join(Index.from_disk(args.secrets).keys()))
+    print("\n".join(Index.from_disk(args["--secrets"]).keys()))
     return 0
 
 def show_secret(args):
     return subprocess.call(["gpg", "-q", "-d",
-                            Index.from_disk(args.secrets)[args.name]])
+                            Index.from_disk(args["--secrets"])[args["<secret>"]]])
 
 def copy_secret(args):
-    with subprocess.Popen(["gpg", "-q", "-d", Index.from_disk(args.secrets)[args.name]],
+    with subprocess.Popen(["gpg", "-q", "-d",
+                           Index.from_disk(args["--secrets"])[args["<secret>"]]],
                           stdout=subprocess.PIPE) as gpg:
         with subprocess.Popen(["head -q -n 1 | tr -d '\n' | xclip -selection clipboard"],
                               shell=True,
@@ -105,10 +115,11 @@ def edit_secret(args):
     if os.getenv("EDITOR") is None:
         print("please set EDITOR", file=sys.stderr)
         return 1
-    with Index.from_disk(args.secrets) as index:
-        if args.name not in index:
-            index.add(args.name)
-        secret_file = index[args.name]
+    with Index.from_disk(args["--secrets"]) as index:
+        secret = args["<secret>"]
+        if secret not in index:
+            index.add(secret)
+        secret_file = index[secret]
     dir = None
     shm = os.path.abspath(os.path.join(os.sep, "dev", "shm"))
     if os.path.isdir(shm):
@@ -120,54 +131,22 @@ def edit_secret(args):
         return subprocess.call(["gpg", "-q", "-e", "-a", "--output", secret_file,
                                 temp_file.name])
 
-def argument_secrets(parser):
-    default = os.getenv("SACRET_DIR", default=os.path.expanduser("~/.sacret"))
-    parser.add_argument("-s", "--secrets",
-                        help="directory of secrets",
-                        default=default,
-                        metavar="<directory>")
+def parse_command(arguments, actions):
+    for command in actions:
+        if arguments[command]:
+            return actions[command]
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Plain file secret manager.")
-    parser.set_defaults(command=lambda a: parser.print_help())
-    subparsers = parser.add_subparsers(title="commands",
-                                       description=None,
-                                       metavar="<command>")
-    p = subparsers.add_parser("init",
-                              description="Create an empty index",
-                              help="create an empty index")
-    argument_secrets(p)
-    p.set_defaults(command=lambda args: Index.init(args.sercrets))
-
-    p = subparsers.add_parser("list",
-                              description="List all secrets",
-                              help="list all secrets")
-    argument_secrets(p)
-    p.set_defaults(command=list_secrets)
-
-    p = subparsers.add_parser("show",
-                              description="Show a secret",
-                              help="show a secret")
-    argument_secrets(p)
-    p.add_argument("name", help="name of a secret")
-    p.set_defaults(command=show_secret)
-
-    p = subparsers.add_parser("copy",
-                              description="Copy first line of a secret to clipboard",
-                              help="copy first line of a secret to clipboard")
-    argument_secrets(p)
-    p.add_argument("name", help="name of a secret")
-    p.set_defaults(command=copy_secret)
-
-    p = subparsers.add_parser("edit",
-                              description="Edit a secret",
-                              help="edit a secret")
-    argument_secrets(p)
-    p.add_argument("name", help="name of a secret")
-    p.set_defaults(command=edit_secret)
-
-    args = parser.parse_args()
+    arguments = docopt(__doc__, version="sacret 0.0.1")
+    actions = {
+        "init": lambda args: Index.init(args["--secrets"]),
+        "list": list_secrets,
+        "show": show_secret,
+        "copy": copy_secret,
+        "edit": edit_secret
+    }
+    arguments["--secrets"] = os.path.expanduser(arguments["--secrets"])
     try:
-        sys.exit(args.command(args))
+        sys.exit(parse_command(arguments, actions)(arguments))
     except subprocess.CalledProcessError as e:
         sys.exit(e.returncode)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sacret',
+    version='0.0.1',
+    description='Plain file secret manager',
+    url='https://github.com/rahcola/sacret',
+    author='Jani Rahkola',
+    author_email='jani.rahkola@iki.fi',
+    license='MIT',
+    packages=[],
+    install_requires=['docopt']
+)
